b71032407915c062d50c094c16dfbaf2dc776088
AlexandreLec <alexandre.lecomte@viacesi.fr>
2016-11-15 21:17:20 +0100

    Plusieurs modif
    - renommer le schéma fridtzing
    - version finale du rapport
    - supression experience1
    - ajout du code source

--- Arduino_sensing.ino
+++ Arduino_sensing.ino
@@ -31,6 +31,9 @@
 float results[N];            //-Filtered result buffer
 float freq[N];            //-Filtered result buffer
 int sizeOfArray = N;
+const int LED_RED = 3;    //LED rouge sur la sortie 3
+const int LED_GREEN = 7; 
+const int LED_YELLOW = 5;
 
  
    
@@ -48,20 +51,31 @@ void setup()
   pinMode(9,OUTPUT);        //-Signal generator pin
   pinMode(8,OUTPUT);        //-Sync (test) pin
 
+  pinMode(LED_RED, OUTPUT); //Définition du pin 3 en sortie
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+
   Serial.begin(115200);
 
   for(int i=0;i<N;i++)      //-Preset results
     results[i]=0;         //-+
+   
+  for(int i=0;i<N;i++)      //-Preset results
+    freq[i]=i;         //-+
 }
 
 void loop()
 {
   unsigned int d;
 
+  int indice_max;
+  
+  indice_max=0;;
+
   int counter = 0;
   for(unsigned int d=0;d<N;d++)
   {
-    int v=analogRead(0);    //-Read response signal
+    int v=analogRead(1);    //-Read response signal
     CLR(TCCR1B,0);          //-Stop generator
     TCNT1=0;                //-Reload new frequency
     ICR1=d;                 // |
@@ -69,18 +83,51 @@ void loop()
     SET(TCCR1B,0);          //-Restart generator
 
     results[d]=results[d]*0.5+(float)(v)*0.5; //Filter results
-    Serial.println(TCCR1B);
-    
+    //Serial.println(TCCR1B);
+    //Serial.println(v);
+    if (results[d] > results[indice_max]) //Test si la valeur que l'on vient d'ajouter est le maximum
+      {
+        indice_max = d;   //Sinon elle est plus grande que le maximum cette valeur devient le nouveau maximum
+      }
  //   plot(v,0);              //-Display
  //   plot(results[d],1);
   // delayMicroseconds(1);
+
+  
   }
-  delay(10000);
+  
 
 
-    //PlottArray(1,freq,results); 
+    PlottArray(1,freq,results);
  
 
   TOG(PORTB,0);            //-Toggle pin 8 after each sweep (good for scope)
+
+
+    if(indice_max >= 67 && indice_max <= 85)
+    {
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+    }
+    if(indice_max < 53)
+    {
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, HIGH);
+    }
+    if(indice_max <= 67 && indice_max >= 53)
+    {
+       digitalWrite(LED_RED, LOW);
+       digitalWrite(LED_GREEN, LOW);
+       digitalWrite(LED_YELLOW, HIGH);
+    }
+    if(indice_max > 85)
+    {
+       digitalWrite(LED_RED, HIGH);
+       digitalWrite(LED_GREEN, HIGH);
+       digitalWrite(LED_YELLOW, HIGH);
+    }
+
 }
    
